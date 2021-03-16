753222d79d472111d48860b76c6d3e7722cd6f01
GomaNiroula <goma.niroula@ucdenver.edu>
2021-03-15 19:24:51 -0600

    Fixed the issue with the next button not working

--- js/googlebooks.js
+++ js/googlebooks.js
@@ -1,26 +1,26 @@
-function searchGoogleBooks(){
-    var txtSearch = document.querySelector("#txtSearch")
-    if(txtSearch.value == ""){
-        document.querySelector("#mainDiv").classList.add("hasError")
-        document.querySelector("#mainDiv").innerHTML = "No search keyword provided.."
-        return;
+function searchGoogleBooks() {
+    
+    var txtSearch = document.querySelector("#txtSearch");
+    if (txtSearch.value == "") {
+        document.querySelector("#mainDiv").classList.add("hasError");
+        document.querySelector("#mainDiv").innerHTML = "No search keyboard provided...";
+        return
     }
-    document.querySelector("#mainDiv").classList.remove("hasError")
 
-    fetch('https://www.googleapis.com/books/v1/volumes?q='+ txtSearch.value + "&startIndex=0&maxResults=20")
+    document.querySelector("#mainDiv").classList.remove("hasError");
+
+    fetch('https://www.googleapis.com/books/v1/volumes?q=' + txtSearch.value + "&startIndex=0&maxResults=20&key=AIzaSyAveODishCcgrLnEShVh6Bp2unmkdqcucI")
     .then(response => response.json())
     .then(json => {
-        var list = `<div class="row col-sm-5">Found ${json.totalItems} books </div>
-        <div class="row">`
-        for(let i = 0; i < json.items.length; i++){
+        var list = `<div class="row col-sm-5">found ${json.totalItems} books</div><div class="row" >`
+        for (let i = 0; i < json.items.length; i++) {
             console.log(json.items[i].volumeInfo.title)
             var bookInfo = json.items[i].volumeInfo;
-            var template = `<u1 class="col-sm-12 col-md-6 col-xl-4">`;
-            template += `<a class="list-group-item list-group-item-action active" href="${json.items[i].selfLink}">${bookInfo.title} </a>`
-            template += `<li class="list-group-item">Author(s): ${bookInfo.authors}</li>`
-            template += `<li class="list-group-item">Publisher: ${bookInfo.publisher}</li>`
-            template += `<li class="list-group-item">Published Date: ${bookInfo.publishedDate}</li>`
-            template += '</u1>'
+            var template = `<ul class="col-sm-12 col-md-6 col-xl-4">`;
+            template += `<a class="list-group-item list-group-item-action active" href="${json.items[i].selfLink}">${bookInfo.title}</a>`
+            template += `<li class="list-group-items">${bookInfo.authors}</li>`
+            template += `<li class="list-group-item">${bookInfo.publisher}, ${bookInfo.publishedDate}</li>`
+            template += `</ul>`
             list += template;
         }
         list += `</div>`
--- js/main.js
+++ js/main.js
@@ -1,28 +1,46 @@
-var startingBook =0;
 const app = Vue.createApp({
     data() {
         return {
             keyword: '',
-            result: null
+            result: null,
+            maxResults: 20,
+            pageNumber: 0,
+            totalItems: 0
         }
     },
     methods: {
+        newSearch(){
+            this.pageNumber = 0;
+            this.totalItems = 0;
+            this.searchGoogleBooks();
+        },
         searchGoogleBooks() {
-            fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + '&startIndex=' + startingBook + '&maxResults=20')
+            fetch("https://www.googleapis.com/books/v1/volumes?q=" + this.keyword + "&startIndex=" + this.startIndex + "&maxResults=" + this.maxResults)
                 .then(response => response.json())
-                .then(json => this.result = json )
-                document.querySelector("#pagination").classList.remove("invisible");
+                .then(json => {
+                    this.result = json;
+                    if(this.pageNumber === 0){
+                        this.totalItems = this.result.totalItems;
+                    }
+                });
+        },
+        decrementPage() {
+            if(this.pageNumber > 0){
+                this.pageNumber--;
+            }
+            this.searchGoogleBooks();
+        },
+        incrementPage() {
+            this.pageNumber++;
+            this.searchGoogleBooks()
+        }
+    },
+    computed: {
+        startIndex(){
+            return this.pageNumber*this.maxResults;
+        },
+        pageNumberDisplay(){
+            return this.pageNumber + 1;
         }
     }
-    
-})
-
-function nextP() {
-    startingBook += 30;
-    this.app.searchGoogleBooks;
-    this.app.template
-}
-
-function previousP() {
-    startingBook -= 30;
-}
+});
